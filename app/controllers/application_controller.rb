9c6b534654186c0763375fb9741ca5c8b516f027
Fernando Blat <ferblape@gmail.com>
2011-04-18 13:56:45 +0200

    Some controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  def render_404
+    respond_to do |format|
+      format.html do
+        render :file => "public/404.html", :status => 404, :layout => false
+      end
+      format.json do
+        render :nothing => true, :status => 404
+      end
+    end
+  end
+  
 end

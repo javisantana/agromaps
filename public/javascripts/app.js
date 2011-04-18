2385af40af19df36195f69b1a4d0ccf1a3df1784
Javier Arce <javierarce@vizzuality.com>
2011-04-18 14:41:56 +0200

    added lat, lng & zoom variables

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,8 +1,12 @@
 $(function() {
 
+  var lat = 37.4419;
+  var lng = -122.1419;
+  var zoom = 13;
+
   if (GBrowserIsCompatible()) {
     var map = new GMap2(document.getElementById("map"));
-    map.setCenter(new GLatLng(37.4419, -122.1419), 13);
+    map.setCenter(new GLatLng(lat, lng), zoom);
     map.setUIToDefault();
   }
 
